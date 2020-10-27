81096ba486b3857abf1790d2f468cd0ecbb9ef58
VaggelisKa <e.karavasileiadis@gmail.com>
2020-10-27 12:03:36 +0200

    Extending the user entity and creating a calculate age extension method

--- Entities/AppUser.cs
+++ Entities/AppUser.cs
@@ -1,4 +1,8 @@
-﻿namespace API.Entities
+﻿using System;
+using System.Collections;
+using API.Extensions;
+
+namespace API.Entities
 {
     public class AppUser
     {
@@ -6,5 +10,19 @@
         public string UserName { get; set; }
         public byte[] PasswordHash { get; set; }
         public byte[] PasswordSalt { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public DateTime Created { get; set; } = DateTime.Now;
+        public DateTime LastActive { get; set; } = DateTime.Now;
+        public string Gender { get; set; }
+        public string Bio { get; set; }
+        public string LookingFor { get; set; }
+        public string Skills { get; set; }
+        public string City { get; set; }
+        public ICollection Photos { get; set; }
+
+        public int GetAge()
+        {
+            return DateOfBirth.CalculateAge();
+        }
     }
 }
