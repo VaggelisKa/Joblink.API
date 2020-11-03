c449f90ac3f5a64e61e966e8938beef91fcc8ca8
VaggelisKa <e.karavasileiadis@gmail.com>
2020-11-03 18:30:52 +0200

    Saving profile updates into the API

---
+++ DTOs/MemberUpdateDto.cs
@@ -0,0 +1,10 @@
+﻿namespace API.DTOs
+{
+    public class MemberUpdateDto
+    {
+        public string Bio { get; set; }
+        public string LookingFor { get; set; }
+        public string Skills { get; set; }
+        public string City { get; set; }
+    }
+}
