055699f664a0185f8f25570a353b6173f8258949
VaggelisKa <e.karavasileiadis@gmail.com>
2020-12-07 20:56:19 +0200

    Configuring DbContext and Entities for Identity Framework

--- Data/Seed.cs
+++ Data/Seed.cs
@@ -18,12 +18,9 @@ namespace API.Data
             var users = JsonSerializer.Deserialize<List<AppUser>>(userData);
             foreach (var user in users)
             {
-                using var hmac = new HMACSHA512();
                 user.UserName = user.UserName.ToLower();
-                user.PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes("Password"));
-                user.PasswordSalt = hmac.Key;
-
-                context.Users.Add(user);
+                
+                await context.Users.AddAsync(user);
             }
 
             await context.SaveChangesAsync();
