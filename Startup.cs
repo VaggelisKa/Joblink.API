fd57c37373317372d52cf796fbe52fb98c85937d
VaggelisKa <e.karavasileiadis@gmail.com>
2020-12-14 18:18:31 +0200

    Authenticating signalR reqs on server

--- Startup.cs
+++ Startup.cs
@@ -62,7 +62,10 @@ namespace API
 
             app.UseRouting();
 
-            app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:4200"));
+            app.UseCors(x => x.AllowAnyHeader()
+                .AllowAnyMethod()
+                .AllowCredentials()
+                .WithOrigins("http://localhost:4200"));
 
             app.UseAuthentication();
             app.UseAuthorization();
